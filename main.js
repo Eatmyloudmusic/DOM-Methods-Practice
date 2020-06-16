5847fc7ac2a7b175119223fd0952ae2a8a9c00ec
william_michael_brough <eatmyloudmusic@gmail.com>
2020-06-16 13:43:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//targetBody and create main tag
+//append to the body
+//create button and image and append to main tag
+//create clickHandler for button
+//  'if' clicked then remove 'main' tag
+
+let mainElement = document.querySelector("main")
+// let heading1  = document.createElement("h1")
+
+
+
+
+
+
+
+
+//--END OF LINE
